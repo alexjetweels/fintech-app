d2fca5b56ba6189336b7854f677e3b4e0b791881
Alex <alex@jetweels.com>
2024-08-26 22:39:38 +0700

    feat: index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
 import { useAssets } from 'expo-asset';
-import { Video } from 'expo-av';
-import { View, Text, StyleSheet } from 'react-native';
+import { ResizeMode, Video } from 'expo-av';
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Index() {
   const [assets] = useAssets([require('@/assets/videos/intro.mp4')]);
@@ -8,6 +11,7 @@ export default function Index() {
     <View style={styles.container}>
       {assets && (
         <Video
+          resizeMode={ResizeMode.COVER}
           isMuted
           isLooping
           shouldPlay
@@ -20,6 +24,36 @@ export default function Index() {
         <Text style={styles.headerText}>
           Ready to change the way you money?
         </Text>
+      </View>
+
+      <View style={styles.button}>
+        <Link
+          href={'/login'}
+          asChild
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.dark },
+          ]}
+        >
+          <TouchableOpacity>
+            <Text style={{ color: 'white', fontSize: 20, fontWeight: '500' }}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <Link
+          href={'/sign-up'}
+          asChild
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: 'white' },
+          ]}
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, fontWeight: '500' }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
       </View>
     </View>
   );
@@ -46,5 +80,13 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     color: 'white',
     textTransform: 'uppercase',
+  },
+
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
   },
 });
